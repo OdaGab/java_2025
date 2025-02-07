6c173f5561f807be12d3577b1242f49caf43a1c7
Odair <odavita32@gmail.com>
2025-02-07 16:47:05 -0300

    arquivo 01 Criando arquivo variaveis

--- 01 - Hw/src/comentarios.java
+++ 01 - Hw/src/comentarios.java
@@ -1,17 +1,13 @@
 /**
  * Java doc
- *
  */
 
 public class comentarios {
-
-
     // cometário de uma linha
     // Mais uma linha
     /*
        Comentário de várias linhas
      */
-
     /**
      * comentario java doc
      * docs.oracle.com
