f8adb714e5cff44df6726e7eef8c665a0864e9d6
Odair <odavita32@gmail.com>
2025-02-21 17:18:35 -0300

    exemplo de if ... else

---
+++ src/Ex02.java
@@ -0,0 +1,34 @@
+
+
+public class Ex02 {
+
+    public static void main(String[] args) {
+
+        int num = 20;
+        switch (num){
+
+            case 5: System.out.println("é igual a cinco");
+            break;
+
+            case 10: System.out.println("é igual a dez");
+            break;
+
+            case 20: System.out.println("é igual a vinte");
+            break;
+        }
+        char Letra = 'X';
+        switch (Letra){
+
+            case 'X': System.out.print("é igual X");
+                break;
+
+            case 'u': System.out.print("é igual a u");
+                break;
+
+            case 'c': System.out.print("é igual a c");
+                break;
+        }
+
+    }
+
+}
