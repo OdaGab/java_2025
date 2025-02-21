db288ce6da3690d36a1f9261ef8c3bec10cfa886
Odair <odavita32@gmail.com>
2025-02-21 14:32:17 -0300

    exemplo de if ... else

--- src/Main.java
+++ src/Main.java
@@ -7,7 +7,13 @@
 public class Main {
     public static void main(String[] args) {
 
+        int x = 11;
 
+        if(x == 10){
+            System.out.println("Ola sou igual a Dez");
+        }else {
+            System.out.println("Ola sou Diferente a Dez");
+        }
 
     }
 }
