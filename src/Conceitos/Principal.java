07e2c9b8e30c0ccfc05d28b90311ae59572bd9e8
Odair Gabriel <32102507+OdaGab@users.noreply.github.com>
2025-02-25 21:21:57 -0300

    Update Principal.java

--- src/Conceitos/Principal.java
+++ src/Conceitos/Principal.java
@@ -5,7 +5,7 @@ package Conceitos;
 //O nome do arquivo deve ser igual ao nome da classe
 public class Principal {
 
-    // agora temos a função main (Função princial)
+    // agora temos a função main (Função principal)
     public static void main(String[] args) {
 
         //Para printarmos algo na tela
